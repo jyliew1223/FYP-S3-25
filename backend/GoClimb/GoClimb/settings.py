33091dcc2671e6e359ccf605b5987357135d026c
jiayu1223 <liewjiayu2022@gmail.com>
2025-08-07 20:31:08 +0800

    enviroment update

--- backend/GoClimb/GoClimb/settings.py
+++ backend/GoClimb/GoClimb/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("./etc/secrets/goclimb-39075-firebase-adminsdk-fbsvc-c421a83f25.json")
+cred = credentials.Certificate("/etc/secrets/goclimb-39075-firebase-adminsdk-fbsvc-c421a83f25.json")
 firebase_admin.initialize_app(cred)
 
 # from decouple import config
