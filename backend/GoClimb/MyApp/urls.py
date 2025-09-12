75fdee776954cba0e1f13240c398e13250872323
Jiayu <166906710+jyliew1223@users.noreply.github.com>
2025-09-13 01:25:25 +0800

    added marketing website

--- backend/GoClimb/MyApp/urls.py
+++ backend/GoClimb/MyApp/urls.py
@@ -1,4 +1,8 @@
+
 from django.urls import path
+
+from . import views
+
 from MyApp.Boundary.user_boundary import signup_view
 from MyApp.Boundary.auth_boundary import verify_id_token_view, verify_app_check_token_view
 from MyApp.Boundary.crag_info import crag_info_view, crag_monthly_ranking_view, crag_trending_view
@@ -7,20 +11,16 @@ from MyApp.Boundary.climb_stats import get_user_climb_stats_view
 
 
 urlpatterns = [
+    path('', views.home, name='home'),
+        
     path('signup/', signup_view, name='user_signup'),
     path('verify_id_token/', verify_id_token_view, name='Verify ID Token'),
     path('verify_app_check_token/', verify_app_check_token_view, name='Verify App Check Token'),
 
-    ### 
-    # wei rong START edit
-    ###
     path('crag_info/', crag_info_view, name='crag_info'),
     path('crag_monthly_ranking/', crag_monthly_ranking_view, name='crag_monthly_ranking'),
     path('crag_trending/', crag_trending_view, name='crag_trending'),  
-    ### 
+    
     path('climb_logs/', get_user_climb_logs_view, name='climb_logs'),
     path('climb_stats/', get_user_climb_stats_view, name='climb_stats'),
-    ### 
-    # wei rong END edit
-    ###
 ]
