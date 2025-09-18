2c95ad2ca7da06f8290a84129682142e30e81719
Jiayu <166906710+jyliew1223@users.noreply.github.com>
2025-09-19 02:50:54 +0800

    minor update

--- backend/GoClimb/GoClimb/settings.py
+++ backend/GoClimb/GoClimb/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]           # ✅ correct
+# STATICFILES_DIRS = [BASE_DIR / "static"]           # ✅ correct
 STATIC_ROOT = BASE_DIR / "staticfiles"            # use / instead of os.path.join
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
