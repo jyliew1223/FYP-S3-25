2d320c58c04525592036ae229131147e6f7c0333
Jiayu <166906710+jyliew1223@users.noreply.github.com>
2025-10-17 14:13:11 +0800

    Update admin_url.py

--- Backend/GoClimb/MyApp/Url/admin_url.py
+++ Backend/GoClimb/MyApp/Url/admin_url.py
@@ -2,10 +2,6 @@
 from django.urls import path
 from MyApp.Boundary.user_admin import suspend_profile_view
 
-from MyApp.Boundary.user_admin import (
-    suspend_profile_view,
-)
-
 urlpatterns = [
     path("suspend_profile/", suspend_profile_view, name="suspend_profile"),   # ADMIN - 2
 ]
